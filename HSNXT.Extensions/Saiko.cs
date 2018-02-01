e415f8724d0ee38f91bff28c5feff1e5eb96ae9f
uwx <cadrekucra@gmx.com>
2018-02-01 21:49:49 -0200

    add docs for saiko

--- HSNXT.Extensions/Saiko.cs
+++ HSNXT.Extensions/Saiko.cs
@@ -7,11 +7,23 @@ namespace HSNXT
     // Extension methods by NaamloosDT, from https://github.com/NaamloosDT/Saiko
     public static partial class Extensions
     {
+        /// <summary>
+        /// Returns a new string in which all the characters in the current instance, beginning at a specified position
+        /// and continuing through the last position, have been deleted. If the specified position is out of bounds,
+        /// returns an unmodified string.</summary>
+        /// <param name="input">The string</param>
+        /// <param name="index">The zero-based position to begin deleting characters. </param>
+        /// <returns>A new string that is equivalent to this string except for the removed characters.</returns>
         public static string TryRemove(this string input, int index)
         {
             return input.Length > index ? input.Remove(index) : input;
         }
 
+        /// <summary>
+        /// Gets the headers for this Url, and returns whether its mime-type is an image or not.
+        /// </summary>
+        /// <param name="url">The url to check.</param>
+        /// <returns>True if the server-returned mime-type starts with "image/", false otherwise.</returns>
         public static bool IsImageUrl(this string url)
         {
             var req = (HttpWebRequest) WebRequest.Create(url);
