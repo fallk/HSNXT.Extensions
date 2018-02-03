eba3092c4dc6ab674a23a8f6c5921625edcec1e9
uwx <cadrekucra@gmx.com>
2018-02-03 01:36:28 -0200

    perf

--- HSNXT.Extensions/SuccincT.JSON/JSON/TypeExtensions.cs
+++ HSNXT.Extensions/SuccincT.JSON/JSON/TypeExtensions.cs
@@ -5,11 +5,13 @@ namespace HSNXT.SuccincT.JSON
 {
     internal static class TypeExtensions
     {
-        internal static bool IsGenericType(this Type type) => type.GetTypeInfo().IsGenericType;
+        internal static bool IsGenericType(this Type type) => type.IsGenericType;
 
+#if NetFX
         internal static MethodInfo GetMethod(this Type type, string name) => type.GetTypeInfo().GetDeclaredMethod(name);
 
         internal static PropertyInfo GetProperty(this Type type, string name) =>
             type.GetTypeInfo().GetDeclaredProperty(name);
+#endif
     }
 }
